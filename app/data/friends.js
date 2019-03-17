deba97903b044b32666d8d2e57e759a06e2ed1b5
Monty <tuckerjsarah@gmail.com>
2019-03-17 13:12:48 -0400

    array of friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,17 +1,132 @@
 // for data on friends as an array
-var friendEx= {
-    "name": "Jane Doe",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-    ]
-}
+const friends=[ 
+    {
+        "name": "Lana Kane",
+        "photo": "https://i.pinimg.com/originals/cc/f7/31/ccf7312946d2f6ac2081e35bcbe2b1e7.jpg",
+        "scores": [
+            1,
+            3,
+            5,
+            4,
+            1,
+            4,
+            5,
+            4,
+            1,
+            1
+        ]
+
+    },
+    {
+        "name": "Cheryl Tunt",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwjuQjsnNrB20bq1N-ReJgZ4cSlCZwxJNC1vv3eu4Sod3vEbReCA",
+        "scores":[
+            1,
+            1,
+            5,
+            1,
+            4,
+            1,
+            5,
+            1,
+            2,
+            4
+        ]
+    }, 
+    {
+        "name": "Pam Poovey",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRl6Ig1UpBsayXCTsx4tOrDZ-j4vsZ2Zq7ipcdM2RQBe5wNj2Sd",
+        "scores": [
+            2,
+            5,
+            4,
+            1,
+            1,
+            4,
+            5,
+            4,
+            3,
+            1
+        ]
+    }, 
+    {
+        "name": "Dr Krieger",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS72VdyfTlnSfVa8uNIVdQeFl-Dr6zsJP8b-2yDOlfMpqcZrKNcTQ",
+        "scores": [
+            3,
+            2,
+            3,
+            2,
+            1,
+            2,
+            2,
+            3,
+            5,
+            4
+        ]
+    },
+    {
+        "name": "Cyril Figgis",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyV5x6Y88ClqQsijnNPnLbm2DFAlEHv9IoM7IbqiaSH8KDla5D",
+        "scores": [
+            5,
+            5,
+            1,
+            4,
+            3,
+            3,
+            2,
+            4,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Malory Archer",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWcPW9upy5Ex7cVmFzFjiCSaT4u72XeKGyr1T9-tWk0bRBQMhv",
+        "scores": [
+            1,
+            1,
+            5,
+            2,
+            3,
+            2,
+            5,
+            5,
+            1,
+            3
+        ]
+    },
+    {
+        "name": "Sterling Archer",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvOiVPeEYG8vTglj0PS0DGsOnYMTPb0LDASZ4HRmvQVbvuFbxNxg",
+        "scores": [
+            1,
+            2,
+            5,
+            1,
+            3,
+            2,
+            5,
+            5,
+            1,
+            1
+        ]
+    }, 
+    {
+        "name": "Ray Gillette",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvNum_N5o4zWtykRKBIZWKkBpy-CBXZxnVHpcXKZDPy-bMjib1FA",
+        "scores": [
+            5,
+            2,
+            3,
+            2,
+            3,
+            4,
+            2,
+            5,
+            1,
+            2
+        ]
+    }
+]
